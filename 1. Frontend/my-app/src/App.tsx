fb40130844689cbd7b0418e5692c9276f7c98493
cornyyumi <cornyyumi@gmail.com>
2022-07-25 21:46:54 +1200

    added basic layout of search form and API call

--- 1. Frontend/my-app/src/App.tsx
+++ 1. Frontend/my-app/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [cocktailName, setCocktailName] = useState("");
+  
+  const COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Title</h1>
+
+
+
+      <div>
+        <input onChange={e => setCocktailName(e.target.value)}></input>
+        <button onClick={search}>
+          Search
+        </button>
+      </div>
+
+
+      <p>
+        You have entered {cocktailName}
+      </p>
     </div>
   );
+
+  function search(){
+    axios.get(COCKTAIL_BASE_URL + "/search.php?s=" + cocktailName).then((res) => {
+      console.log(res.data);
+    });
+  }
 }
+
 
 export default App;
